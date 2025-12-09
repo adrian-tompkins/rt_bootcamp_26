bfa8568d7e4a2569c28ad64f1369de3f8a239861
adrian-tompkins <adrian.lee.tompkins@gmail.com>
2025-12-09 06:52:27 +0000

    Reworked streaming content to use shared source location

--- Data Engineering with Lakeflow/Pipeline/ingest.py
+++ Data Engineering with Lakeflow/Pipeline/ingest.py
@@ -1,10 +1,6 @@
 from pyspark import pipelines as dp
 import pyspark.sql.functions as F
 
-
-user = spark.conf.get("user")
-assert user != "" and user is not None, "Set user in the pipeline config"
-
 # Step 1: Auto Load Raw JSON Data into Multiplex Bronze
 @dp.table()
 def sdp_bronze():
@@ -13,5 +9,5 @@ def sdp_bronze():
         .format("cloudFiles")
         .option("cloudFiles.format", "json")
         .schema("key BINARY, value BINARY, topic STRING, partition LONG, offset LONG, timestamp LONG")
-        .load(f"/Volumes/rtlh_lakehouse_labs/labs_{user}/lab2/resources/data/gym")
+        .load(f"/Volumes/rtlh_lakehouse_labs/bootcamp_oct_2025/resources/data/gym_stream/")
     )
